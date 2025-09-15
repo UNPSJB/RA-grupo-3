7643af1c25129d3c9b9a79f510e5ba29f5840eba
Marcelo Rodriguez <marcezro8@gmail.com>
2025-09-15 16:33:01 -0300

    probando como funciona .gitignore

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,4 +3,18 @@ from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from src.database import engine
+from src.models import ModeloBase
 
+load_dotenv()
+
+ENV = os.getenv("ENV")
+ROOT_PATH = os.getenv(f"ROOT_PATH_{ENV.upper()}")
+
+
+@asynccontextmanager
+async def db_creation_lifespan(app: FastAPI):
+    ModeloBase.metadata.create_all(bind=engine)
+    yield
+
+
+app = FastAPI(root_path=ROOT_PATH, lifespan=db_creation_lifespan)
