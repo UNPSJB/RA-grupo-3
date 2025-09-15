41ac4c06ada004ccb6ebf0a9856cfaa495ebe9c3
Marcelo Rodriguez <marcezro8@gmail.com>
2025-09-15 17:52:05 -0300

    Ya esta todo para crear la base de datos

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from src.database import engine
 from src.models import ModeloBase
 
+
 load_dotenv()
 
 ENV = os.getenv("ENV")
@@ -18,3 +19,4 @@ async def db_creation_lifespan(app: FastAPI):
 
 
 app = FastAPI(root_path=ROOT_PATH, lifespan=db_creation_lifespan)
+
