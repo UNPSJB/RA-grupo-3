641e242e6977a9cc3398a32d6da8de41a6a8784a
Toxicoria <coria.augusto.ram@gmail.com>
2025-09-16 23:56:04 -0300

    agregue anio_carrera para saber a que año corresponde la encuesta y enum de primer/segundo/anual cuatrimestre...

--- backend/src/encuestas/models.py
+++ backend/src/encuestas/models.py
@@ -1,17 +1,38 @@
 from datetime import datetime
-
-from sqlalchemy import Integer, String, DateTime, Boolean #agregado datetime y boolean
-
+from sqlalchemy import Integer, String, DateTime, Boolean, Enum, CheckConstraint #agregado datetime, Enum, checkconstraint y boolean
 from sqlalchemy.sql import func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.models import ModeloBase
 
+from enum import Enum
+
+
+class TipoCuatrimestre(str, Enum):
+    PRIMERO = "primero"
+    SEGUNDO = "segundo"
+    ANUAL = "anual"
+
 class Encuesta(ModeloBase):
     __tablename__ = "encuesta" #cambio de nombre a encuesta singular por un tema de buenas practicas
+
+
+    # filtro para saber en que año de la carrera pertenece la encuesta
+    # restringido a poner entre 1 y 6 años (osea primero a sexto por las dudas)
+    __table_args__ = (
+        CheckConstraint("anio_carrera BETWEEN 1 AND 6", name="ck_encuesta_anio_carrera"),
+        )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     titulo: Mapped[str] = mapped_column(String, index=True)
     descripcion: Mapped[str] = mapped_column(String, index=True)
+
+# año para filtrar por que año de la carrera corresponde la encuesta
+    anio_carrera: Mapped[int] = mapped_column(Integer, nullable=False)
+
+# para filtrar por cuatrimestre
+    cursada: Mapped[TipoCuatrimestre] = mapped_column(
+        Enum(TipoCuatrimestre, name="cuatrimestre_enum"), nullable=False
+    )
 
 # fecha de inicio y de fin para cuando se pone activa la encuesta o se cierra
     fecha_inicio: Mapped[datetime] = mapped_column(
@@ -21,6 +42,5 @@ class Encuesta(ModeloBase):
     
 # para saber si esta completa la encuesta
     esta_completa: Mapped[bool] = mapped_column(Boolean, default=False)
- 
  
     # Faltaria la relación con las preguntas
