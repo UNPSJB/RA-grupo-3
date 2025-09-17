c968eda1c1de9d614f89b73297928bdc6be56909
Toxicoria <coria.augusto.ram@gmail.com>
2025-09-16 23:31:55 -0300

    atributo bool para saber si esta completa la encuesta

--- backend/src/encuestas/models.py
+++ backend/src/encuestas/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Integer, String, DateTime #agregado datetime
+from sqlalchemy import Integer, String, DateTime, Boolean #agregado datetime y boolean
 
 from sqlalchemy.sql import func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -15,9 +15,12 @@ class Encuesta(ModeloBase):
 
 # fecha de inicio y de fin para cuando se pone activa la encuesta o se cierra
     fecha_inicio: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=func.now())
+        DateTime(timezone=True), default=func.now()) # esto deja la fecha de creacion automaticamente.
     fecha_fin: Mapped[datetime | None] = mapped_column(
-        DateTime(timezone=True), nullable=True)
+        DateTime(timezone=True), nullable=True) # es nulleable ya que no conocemos cuando va a cerrar la encuesta
     
-     
+# para saber si esta completa la encuesta
+    esta_completa: Mapped[bool] = mapped_column(Boolean, default=False)
+ 
+ 
     # Faltaria la relación con las preguntas
