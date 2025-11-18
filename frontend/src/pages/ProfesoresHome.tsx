491c3ea4883fec3308b339231173a7069973f372
Toxicoria <coria.augusto.ram@gmail.com>
2025-11-18 17:40:46 -0300

    ultimos cambios

--- frontend/src/pages/ProfesoresHome.tsx
+++ frontend/src/pages/ProfesoresHome.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import  TarjetasInfo  from '../components/TarjetasInfo.tsx'
+import TarjetasInfo from '../components/TarjetasInfo.tsx';
+import DashboardWidgets from '../components/DashboardWidgets.tsx';
+
 const ProfesoresHome: React.FC = () => {
   return (
-    <div className="container mx-auto px-4">
+    // max-w-6xl mantiene todo alineado y centrado
+    <div className="container mx-auto px-4 py-10 max-w-6xl">
+
+      {/* Fila 1: Widgets Rectangulares (Grid 2 cols) */}
+      <DashboardWidgets />
+      
+      {/* Fila 2: Tarjetas Cuadradas/Verticales (Grid 3 cols) */}
       <TarjetasInfo />
     </div>
   );
